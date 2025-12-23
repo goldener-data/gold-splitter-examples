07422bd7a3feacf5f38c62dbc6f544f13d34e7d4
Yannn-CV <yann.chene.tni@gmail.com>
2025-12-23 15:57:10 +0000

    update random run

--- image_classification_cifar10/run/dinov3small_224_random.sh
+++ image_classification_cifar10/run/dinov3small_224_random.sh
@@ -1,25 +1,25 @@
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=42 random_split_state = 42
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=53 random_split_state = 42
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=64 random_split_state = 42
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=75 random_split_state = 42
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=86 random_split_state = 42
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=42 random_split_state = 53
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=53 random_split_state = 53
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=64 random_split_state = 53
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=75 random_split_state = 53
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=86 random_split_state = 53
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=42 random_split_state = 64
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=53 random_split_state = 64
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=64 random_split_state = 64
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=75 random_split_state = 64
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=86 random_split_state = 64
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=42 random_split_state = 75
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=53 random_split_state = 75
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=64 random_split_state = 75
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=75 random_split_state = 75
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=86 random_split_state = 75
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=42 random_split_state = 86
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=53 random_split_state = 86
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=64 random_split_state = 86
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=75 random_split_state = 86
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=gold gold_splitter.update_selection=True random_state=86 random_split_state = 86
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=42 random_split_state=42
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=53 random_split_state=42
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=64 random_split_state=42
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=75 random_split_state=42
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=86 random_split_state=42
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=42 random_split_state=53
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=53 random_split_state=53
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=64 random_split_state=53
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=75 random_split_state=53
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=86 random_split_state=53
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=42 random_split_state=64
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=53 random_split_state=64
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=64 random_split_state=64
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=75 random_split_state=64
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=86 random_split_state=64
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=42 random_split_state=75
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=53 random_split_state=75
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=64 random_split_state=75
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=75 random_split_state=75
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=86 random_split_state=75
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=42 random_split_state=86
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=53 random_split_state=86
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=64 random_split_state=86
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=75 random_split_state=86
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random random_state=86 random_split_state=86
