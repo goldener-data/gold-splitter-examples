73002c477dae0da53b3ba020f6e4e01bdc749a75
Yannn-CV <yann.chene.tni@gmail.com>
2026-01-20 21:52:27 +0000

    new run files

--- image_classification_cifar10/run/dinov3small_224_both.sh
+++ image_classification_cifar10/run/dinov3small_224_both.sh
@@ -1,5 +1,5 @@
 uv run python -m image_classification_cifar10.cifar10_experiment split_method=both gold_splitter.update_selection=True random_state=42 random_split_state=42
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=random gold_splitter.update_selection=True random_state=42 random_split_state=53
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=random gold_splitter.update_selection=True random_state=42 random_split_state=64
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=random gold_splitter.update_selection=True random_state=42 random_split_state=75
-uv run python -m image_classification_cifar10.cifar10_experiment split_method=random gold_splitter.update_selection=True random_state=42 random_split_state=86
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random gold_splitter.update_selection=False drop_duplicate_table=False random_state=42 random_split_state=53
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random gold_splitter.update_selection=False drop_duplicate_table=False random_state=42 random_split_state=64
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random gold_splitter.update_selection=False drop_duplicate_table=False random_state=42 random_split_state=75
+uv run python -m image_classification_cifar10.cifar10_experiment split_method=random gold_splitter.update_selection=False drop_duplicate_table=False random_state=42 random_split_state=86
