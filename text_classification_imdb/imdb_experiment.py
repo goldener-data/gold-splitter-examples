44a737bfe2422bd6e4036076c4dee8d1c97a97c6
Yann-CV <yann.chene.tni@gmail.com>
2026-03-19 16:05:46 +0000

    fix indices

--- text_classification_imdb/imdb_experiment.py
+++ text_classification_imdb/imdb_experiment.py
@@ -80,14 +80,20 @@ def run_experiment(
         artifact_file="config.yaml",
     )
 
-    mlflow_logger.experiment.log_dict(
-        run_id=mlflow_logger.run_id,
-        dictionary={
+    if split_method == "gold":
+        indices_dict = {
             "gold_train_indices": data_module.gold_train_indices,
             "gold_val_indices": data_module.gold_val_indices,
+        }
+    else:
+        indices_dict = {
             "sk_train_indices": data_module.sk_train_indices,
             "sk_val_indices": data_module.sk_val_indices,
-        },
+        }
+
+    mlflow_logger.experiment.log_dict(
+        run_id=mlflow_logger.run_id,
+        dictionary=indices_dict,
         artifact_file="indices.json",
     )
 
